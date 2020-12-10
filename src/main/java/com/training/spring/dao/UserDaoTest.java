253b0887f80d04434f2495322969787c7832203a
yaans <kbeeysk@naver.com>
2020-12-10 11:42:38 +0900

    [refactoring] 오브젝트들의 생성 주기를 관리하는 IoC 클래스 생성 in DaoFactory.java

--- src/main/java/com/training/spring/dao/UserDaoTest.java
+++ src/main/java/com/training/spring/dao/UserDaoTest.java
@@ -4,14 +4,18 @@ import com.training.spring.domain.User;
 
 import java.sql.SQLException;
 
-// 런타임 오브젝트 관계 구조를 만들어줄 책임을 클라이언트에게 떠넘긴다
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker c = new DConnectionMaker();
-        UserDao dao = new UserDao(c);
+
+        // 기존 코드
+//        ConnectionMaker c = new DConnectionMaker();
+//        UserDao dao = new UserDao(c);
+
+        // Factory 생성 후 -> UserDaoTest.java는 UserDao 테스트에만 충실
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
-        user.setId("testId7");
+        user.setId("te2Id7");
         user.setName("테스트이름");
         user.setPassword("1234");
 
