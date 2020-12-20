3c629217ac50d55a9f9913f747ff49a17c710fbe
yaans <kbeeysk@naver.com>
2020-12-20 15:16:44 +0900

    [refactoring] JUnit을 이용한 테스트 코드 개선 (중복 코드 메소드 추출)

--- src/main/java/com/training/spring/dao/UserDaoTest.java
+++ src/main/java/com/training/spring/dao/UserDaoTest.java
@@ -1,24 +1,36 @@
 package com.training.spring.dao;
 
 import com.training.spring.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTest {
     public static void main(String[] args) {
         JUnitCore.main("com.training.spring.dao.UserDaoTest");
     }
+
+    // 픽스처 : 테스트를 수행하는 데 필요한 오브젝트
+    private UserDao dao;    // 테스트 메소드에서 접근할 수 있도록 인스턴스 변수로 변경
+
+    @Before     // @Test 메소드가 실행되기 전 먼저 실행해야하는 메소드 정의
+    public void setUp(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.dao = context.getBean("userDao", UserDao.class);
+    }
+
     @Test       // JUnit 테스트용 메소드는 반드시 public 선언 & 반환 void
     public void addAndGet() throws SQLException, ClassNotFoundException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));      // 데이터가 모두 삭제되었는지 레코드 수 확인
 
@@ -39,9 +51,6 @@ public class UserDaoTest {
     }
     @Test
     public void count() throws SQLException, ClassNotFoundException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -55,9 +64,6 @@ public class UserDaoTest {
     // 예외상황에 대비한 테스트
     @Test(expected = EmptyResultDataAccessException.class)      // 테스트 도중 발생할 것으로 예상되는 예외 클래스 지정
     public void getUserFailure() throws SQLException, ClassNotFoundException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
