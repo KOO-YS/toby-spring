8692032fe18e679d3cf78bedcd2ab9ec85de4b48
yaans <kbeeysk@naver.com>
2020-12-26 22:43:28 +0900

    [refactoring] 전략 인터페이스를 로직별로 클래스 구현하는 대신, UserDao 메소드 안에 익명 내부 클래스로 변환해 구현

--- src/main/java/com/training/spring/dao/UserDao.java
+++ src/main/java/com/training/spring/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.training.spring.dao;
 
 import com.training.spring.domain.User;
+import com.training.spring.strategy.AddStatement;
 import com.training.spring.strategy.DeleteAllStatement;
 import com.training.spring.strategy.StatementStrategy;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -24,42 +25,32 @@ public class UserDao {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            ps = c.prepareStatement("INSERT INTO users(id, name, password) values(?, ?, ?)");
+//            StatementStrategy st = new AddStatement(user);           // 전략 오브젝트에 추가적인 User 정보 전달 !
+        // 익명 클래스로 바로 구현
+        StatementStrategy st = new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?)");
 
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-
-            ps.executeUpdate();
-
-            ps.executeUpdate();
-
-        } catch (SQLException e){
-            throw e;
-
-        } finally {
-            if(ps != null){
-                try {
-                    ps.close();
-                } catch (SQLException e){
-                    throw e;
-                }
+                ps.setString(1, user.getId());          // user 정보를 따로 선언하지 않아도 된다
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
             }
-            if(c != null){
-                try {
-                    c.close();
-                } catch (SQLException e){
-                    throw e;
-                }
-            }
-        }
+        };
+            jdbcContextWithStatementStrategy(st);
     }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-        StatementStrategy st = new DeleteAllStatement();        // 전략 오브젝트 생성
+//        StatementStrategy st = new DeleteAllStatement();        // 전략 오브젝트 생성
+        // 익명 클래스를 이용한 바로 구현
+        StatementStrategy st = new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("DELETE FROM users");
+                return ps;
+            }
+        };
         jdbcContextWithStatementStrategy(st);                   // 컨텍스트는 전략 오브젝트 제공받음
     }
 
@@ -69,6 +60,7 @@ public class UserDao {
         ResultSet rs = null;
         User user = null;
         try {
+            c = connectionMaker.makeNewConnection();
             ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
             ps.setString(1, id);
 
@@ -119,6 +111,7 @@ public class UserDao {
         ResultSet rs = null;
         int count = 0;
         try {
+            c = connectionMaker.makeNewConnection();
             ps = c.prepareStatement("SELECT COUNT(*) FROM users");
 
             rs = ps.executeQuery();
