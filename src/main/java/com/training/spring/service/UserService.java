035fae2a27065bc5be1d0fa41a352fa7403fbb9e
yaans <kbeeysk@naver.com>
2021-01-07 14:35:40 +0900

    [refactoring] 사용자 레벨 관리 기능 기본 구현 in UserService

--- src/main/java/com/training/spring/service/UserService.java
+++ src/main/java/com/training/spring/service/UserService.java
@@ -1,6 +1,10 @@
 package com.training.spring.service;
 
 import com.training.spring.dao.UserDao;
+import com.training.spring.domain.Level;
+import com.training.spring.domain.User;
+
+import java.util.List;
 
 public class UserService {
     UserDao userDao;
@@ -9,4 +13,52 @@ public class UserService {
         this.userDao = userDao;
     }
 
+    public void upgradeLevels(){
+        List<User> users = userDao.getAll();
+        for(User user : users){
+            Boolean changed = null;     // level 변경 플래그
+
+            // BASIC 레벨 업그레이드
+            if(user.getLevel() == Level.BASIC && user.getLogin() >= 50){
+                user.setLevel(Level.SILVER);
+                changed = true;
+            }
+            // SILVER 레벨 업그레이드
+            else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30){
+                user.setLevel(Level.GOLD);
+                changed = true;
+            }
+            else if(user.getLevel() == Level.GOLD){
+                changed = false;
+            }
+            else {
+                changed = false;
+            }
+            if(changed){
+                userDao.update(user);
+            }
+
+        }
+    }
+
+    public boolean canUpgradeLevel(User user){
+        Level currentLevel = user.getLevel();
+        switch (currentLevel){
+            case BASIC:
+                return (user.getLogin() >= 50);
+            case SILVER:
+                return (user.getRecommend() >= 30);
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalArgumentException("unknown Level : "+currentLevel);
+        }
+
+    }
+    public void add(User user){
+        if(user.getLevel() == null){
+            user.setLevel(Level.BASIC);
+        }
+        userDao.add(user);
+    }
 }
--- src/test/java/com/training/spring/service/UserServiceTest.java
+++ src/test/java/com/training/spring/service/UserServiceTest.java
@@ -1,11 +1,19 @@
 package com.training.spring.service;
 
+import com.training.spring.dao.UserDao;
+import com.training.spring.dao.UserDaoJdbc;
+import com.training.spring.domain.Level;
+import com.training.spring.domain.User;
 import com.training.spring.factory.DaoFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -15,14 +23,68 @@ import static org.junit.Assert.assertThat;
 public class UserServiceTest {
     @Autowired
     UserService userService;
+    @Autowired
+    UserDao userDao;
+
+    List<User> userList;
 
     @Before
     public void setUpBean(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         this.userService = context.getBean("userService", UserService.class);
+        this.userDao = context.getBean("userDaoJdbc", UserDaoJdbc.class);
+
     }
+
+    @Before
+    public void setFixture(){
+        userList = Arrays.asList(
+          new User("Apple", "사과", "p1", Level.BASIC, 49, 0),
+          new User("Banana","바나나", "p2", Level.BASIC, 50, 0),
+          new User("Cherry","체리", "p3", Level.SILVER, 60, 29),
+          new User("Date","대추", "p4", Level.SILVER, 60, 30),
+          new User("Egg","계란", "p5",Level.GOLD, 100, 100)
+        );
+    }
+
     @Test
-    public void bean(){
-        assertThat(this.userService, is(notNullValue()));
+    public void upgradeLevels(){
+        userDao.deleteAll();
+
+        for(User u : userList){
+            userDao.add(u);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(userList.get(0), Level.BASIC);
+        checkLevel(userList.get(1), Level.SILVER);
+        checkLevel(userList.get(2), Level.SILVER);
+        checkLevel(userList.get(3), Level.GOLD);
+        checkLevel(userList.get(4), Level.GOLD);
+
+    }
+
+    public void checkLevel(User user, Level expectedLevel){
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+
+    @Test
+    public void add(){
+        userDao.deleteAll();
+
+        User userWithLevel = userList.get(4);
+        User userWithoutLevel = userList.get(0);
+        userWithoutLevel.setLevel(null);    // 레벨이 비어있는 사용자로 설정 -> BASIC으로 자동 설정 요구
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+        assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
     }
 }
